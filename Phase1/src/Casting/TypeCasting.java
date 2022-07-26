3ddee68d6fdb700fd6421a4e6873828d52aba0ea
UmaKaruppaiah <umamaheswari02.k@gmail.com>
2022-07-26 15:30:19 +0530

    assignment

---
+++ Phase1/src/Casting/TypeCasting.java
@@ -0,0 +1,17 @@
+package Casting;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 10;
+	    System.out.println("The integer value: " + num);
+
+	    // convert into double type
+	    double data = num;
+	    System.out.println("The double value: " + data);
+	  
+
+	}
+
+}
